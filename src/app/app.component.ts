6eb4074f23a5074b933fbc9d4833dd70faee626d
Tamara Mistelbauer <tamaramistelbauer@yahoo.com>
2025-03-08 20:42:00 +0100

    add head tag and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  standalone: true,
+  imports: [ContentComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
