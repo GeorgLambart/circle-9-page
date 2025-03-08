c3151d8b472d8231fbe3b62bb19d6d50887228ab
Tamara Mistelbauer <tamaramistelbauer@yahoo.com>
2025-03-08 22:18:35 +0100

    add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContentComponent, NavBarComponent],
+  imports: [NavBarComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
