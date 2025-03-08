0ce2eaca664ec9543656c419629527d66c717acc
Tamara Mistelbauer <tamaramistelbauer@yahoo.com>
2025-03-08 15:49:08 +0100

    remove placeholder in app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'circle-9-page' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('circle-9-page');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
